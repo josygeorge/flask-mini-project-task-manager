fb6098165924b59fbc00600cec6de4cdb42754fd
josygeorge <joe.george.josy@gmail.com>
2021-06-14 04:53:54 +0000

    Added Materialize css and script

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (Flask, flash, render_template, 
+        redirect, request, session, url_for)
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
@@ -7,10 +9,22 @@ if os.path.exists("env.py"):
 # create instance of Flask
 app = Flask(__name__)
 
+app.config["MONGO_DATABASE"] = os.environ.get("MONGO_DATABASE")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.SECRET_KEY = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/get-tasks')
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
